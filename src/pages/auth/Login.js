8f72be9d07d88d96eddc19d740fe6adb5f1966d5
Nathan Kiner <nathankiner@gmail.com>
2023-08-16 12:29:45 +0200

    Replaced dep @supabase/ui (deprecated)

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect } from 'react';
-import { Auth } from '@supabase/ui';
+import { Auth } from '@supabase/auth-ui-react';
 import { AuthContext } from '../../contexts/AuthContext';
 import { createClient } from '@supabase/supabase-js';
 
@@ -7,35 +7,35 @@ const supabaseURL = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export default function Login() {
-    const { session, supabase } = useContext(AuthContext);
-    const [authSession, setAuthSession] = session;
-    const [supaClient, setSupaClient] = supabase;
+  const { session, supabase } = useContext(AuthContext);
+  const [authSession, setAuthSession] = session;
+  const [supaClient, setSupaClient] = supabase;
 
 
-    useEffect(() => {
-        if (!supaClient) {
+  useEffect(() => {
+    if (!supaClient) {
             setSupaClient(createClient(supabaseURL, supabaseKey)
             )
         }
         else {
-            supaClient.auth.getSession().then(({ data: { authSession } }) => {
-                setAuthSession(authSession);
-            });
+      supaClient.auth.getSession().then(({ data: { authSession } }) => {
+        setAuthSession(authSession);
+      });
 
             const { data: { subscription }, } = supaClient.auth.onAuthStateChange((_event, session) => {
-                setAuthSession(authSession);
+        setAuthSession(authSession);
             })
 
             return () => subscription.unsubscribe()
-        }
-    }, [authSession, setAuthSession, setSupaClient, supaClient]);
-    if (!supaClient) {
-        return (<div>Connecting...</div>)
-    } else {
-        if (!session) {
-            return <Auth supabaseClient={supaClient} />;
-        } else {
-            return <div>Logged in!</div>;
-        }
     }
+  }, [authSession, setAuthSession, setSupaClient, supaClient]);
+  if (!supaClient) {
+        return (<div>Connecting...</div>)
+  } else {
+    if (!session) {
+      return <Auth supabaseClient={supaClient} />;
+    } else {
+      return <div>Logged in!</div>;
+    }
+  }
 }
