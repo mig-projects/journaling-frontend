c417e4b282c715d580e43981d8380d88cb45e04f
Nathan Kiner <nathankiner@gmail.com>
2023-08-16 16:04:37 +0200

    Separated effect and changed condition to login #2

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,7 +1,7 @@
-import React, { useContext, useEffect } from 'react';
-import { Auth } from '@supabase/auth-ui-react';
-import { AuthContext } from '../../contexts/AuthContext';
-import { createClient } from '@supabase/supabase-js';
+import React, { useContext, useEffect } from "react";
+import { Auth } from "@supabase/auth-ui-react";
+import { AuthContext } from "../../contexts/AuthContext";
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseURL = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
@@ -11,29 +11,32 @@ export default function Login() {
   const [authSession, setAuthSession] = session;
   const [supaClient, setSupaClient] = supabase;
 
-
+  // Initialize client
   useEffect(() => {
     if (!supaClient) {
-            setSupaClient(createClient(supabaseURL, supabaseKey)
-            )
-        }
-        else {
-      supaClient.auth.getSession().then(({ data: { authSession } }) => {
-        setAuthSession(authSession);
-      });
-
-            const { data: { subscription }, } = supaClient.auth.onAuthStateChange((_event, session) => {
-        setAuthSession(authSession);
-            })
-
-            return () => subscription.unsubscribe()
+      setSupaClient(createClient(supabaseURL, supabaseKey));
     }
-  }, [authSession, setAuthSession, setSupaClient, supaClient]);
+  }, [supaClient]);
+
+  // Listen to change on Auth State
+  useEffect(() => {
+    if (!supaClient) return;
+
+    const {
+      data: { subscription },
+    } = supaClient.auth.onAuthStateChange((_event, newSession) => {
+      setAuthSession(newSession);
+    });
+
+    return () => subscription.unsubscribe();
+  }, [supaClient]);
   if (!supaClient) {
-        return (<div>Connecting...</div>)
+    return <div>Connecting...</div>;
   } else {
-    if (!session) {
-      return <Auth supabaseClient={supaClient} />;
+    if (!authSession) {
+      return (
+        <Auth supabaseClient={supaClient} providers={["google", "facebook"]} />
+      );
     } else {
       return <div>Logged in!</div>;
     }
